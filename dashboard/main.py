4c3ecfe43c988c72da6312a73655827e54355ef2
Andreas Walser <andreas.walser@sbb.ch>
2025-06-24 14:19:37 +0200

    added tabs

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,23 +1,38 @@
 import streamlit as st
-import pandas as pd
 from tabs import tab1, tab2
 from load_data import load_data
 
+
 def main():
-    st.set_page_config(page_title="Hello World Streamlit App", layout="wide")
+    st.set_page_config(page_title="Charged", layout="wide")
     st.title("Hello World Streamlit App")
     # Initialize shared dataframe in session state if not present
     if "shared_df" not in st.session_state:
         st.session_state["shared_df"] = load_data()
-    tabs = st.tabs(["Tab 1", "Tab 2"])
+    tabs = st.tabs(["Overview", "Map", "Time Series", "Errors", "Capacity"])
+    mapping = {
+        "Overview": "KPI overview",
+        "Map": "Map",
+        "TimeSeries": "Time Series",
+        "Errors": "Errors",
+        "Capacity": "Capacity",
+    }
     with tabs[0]:
-        st.subheader("Tab 1: Full DataFrame")
-        st.dataframe(st.session_state["shared_df"])
+        st.subheader(mapping["Overview"])
         tab1.render()
     with tabs[1]:
-        st.subheader("Tab 2: First 2 Rows of DataFrame")
-        st.dataframe(st.session_state["shared_df"].head(2))
+        st.subheader(mapping["Map"])
         tab2.render()
+    with tabs[2]:
+        st.subheader(mapping["TimeSeries"])
+        tab2.render()
+    with tabs[3]:
+        st.subheader(mapping["Errors"])
+        tab2.render()
+    with tabs[4]:
+        st.subheader(mapping["Capacity"])
+        tab2.render()
+
 
 if __name__ == "__main__":
     main()
